ca75c6936714a20d384f8defd9c7a8b94ad4fe4d
Indroneel007 <2021ume1106@mnit.ac.in>
2025-08-06 22:59:17 +0530

    first commit

---
+++ client/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="min-h-scree w-screen border-red-400">
+        
+      </div>
+    </div>
+    
+  );
+}
